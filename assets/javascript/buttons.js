04917ab61cffa010d5a5bb1ea5861177740ebd3d
Ramón Díaz <jramondiaz96@gmail.com>
2020-04-09 20:23:32 -0500

    buscador y arreglos de detalles
    falta responsividad y los efectos js

---
+++ assets/javascript/buttons.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $(".btn-categorias").click(function() {
+    $(".btn-categorias").removeClass("active-buttons");
+    $(this).addClass("active-buttons");
+  });
+});
